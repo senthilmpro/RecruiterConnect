40e0366b0e7ee1eeb0b53b65da38a26ba2e90756
Senthil Kumar M <m.senthil.000@gmail.com>
2015-04-13 05:32:22 -0500

    RecruiterConnect
    Page Edit

---
+++ server.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var app = express();
+var http = require('http');
+
+
+var redirectURL = "http://localhost:3000";
+var consumerAPIKey = "78pfcenlidiqv7";
+var consumerSecretKey = "Duf8HN5wN5y0qJ12";
+
+
+//Request parameters
+var start=0;
+var count=500;
+var modified=new
+var format=json;
+
+//Get authentication information.
+http.get('/',function(req,res) {
+
+
+
+});
+
